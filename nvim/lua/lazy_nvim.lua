d2d2e60a63155dd7a73c637aa10ab116936bdb0c
muzichangqing <code_lcq@163.com>
2023-07-22 12:10:33 +0800

    add lazy.nvim & tokonight theme

---
+++ nvim/lua/lazy_nvim.lua
@@ -0,0 +1,27 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+    -- 主题插件
+    {
+        "folke/tokyonight.nvim",
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            -- load the colorscheme here
+            vim.cmd([[colorscheme tokyonight]])
+        end,
+    },
+}
+local opts = {}
+require("lazy").setup(plugins, opts)
